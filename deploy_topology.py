4dd9fde0db2b0d2775f900739cd0878302c30dd8
Joachim de Groot <joachim.de.groot@menzel-it.net>
2025-05-14 11:17:14 +0200

    Some liniting

--- deploy_topology.py
+++ deploy_topology.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 """
-This script creates a GNS3 project, adds nodes, interconnect and boots all of them, then
-applies Day-0 configuration to them. It also creates an Ansible inventory that can be
-used for further configuration.
+This script creates a GNS3 project, adds nodes, interconnect and boots all of
+them, then applies Day-0 configuration to them. It also creates an Ansible
+inventory that can be used for further configuration.
 """
 
 from json import dumps
@@ -13,35 +13,36 @@ from re import sub
 from requests import get, post, delete
 from yaml import safe_dump, load
 
+
 def create_project(name):
     """
     Checking if a project with a given name already exists; if yes, deleting it.
     Then the function (re)creates the project and returns the project ID.
     """
 
-    ### Finding the project ID if a project with the given name exists.
+    # Finding the project ID if a project with the given name exists.
     url = "http://%s:%s/v2/projects" % (CONFIG["gns3_server"], CONFIG["gns3_port"])
     response = get(url)
     if response.status_code == 200:
         body = response.json()
         project = next((item for item in body if item["name"] == CONFIG["project_name"]), None)
     else:
-        print("Received HTTP error %d when checking if the project already exists! Exiting." % \
-               response.status_code)
+        print("Received HTTP error %d when checking if the project already exists! Exiting." %
+              response.status_code)
         exit(1)
 
-    ### Deleting the project if it already exists.
-    if project != None:
+    # Deleting the project if it already exists.
+    if project is not None:
         delete_project_id = project["project_id"]
         url = "http://%s:%s/v2/projects/%s" % \
               (CONFIG["gns3_server"], CONFIG["gns3_port"], delete_project_id)
         response = delete(url)
         if response.status_code != 204:
-            print("Received HTTP error %d when deleting the existing project! Exiting." \
+            print("Received HTTP error %d when deleting the existing project! Exiting."
                   % response.status_code)
             exit(1)
 
-    ### (Re)creating the project
+    # (Re)creating the project
     url = "http://%s:%s/v2/projects" % (CONFIG["gns3_server"], CONFIG["gns3_port"])
     data = {"name": name}
     data_json = dumps(data)
@@ -53,7 +54,6 @@ def create_project(name):
     else:
         print("Received HTTP error %d when creating the project! Exiting." % response.status_code)
         exit(1)
-
 
 
 def assign_appliance_id():
@@ -77,21 +77,20 @@ def assign_appliance_id():
         exit(1)
 
 
-
 def add_nodes():
     """
     This function adds a node to the project already created.
     """
 
-    ### Adding nodes
+    # Adding nodes
     for appliance in CONFIG["nodes"]:
         instance_seq = 1
         for instance in appliance["instances"]:
-            ### Adding node name to the config
+            # Adding node name to the config
             instance["name"] = appliance["appliance_name"].replace(" ", "") + \
                                "-"  + str(instance_seq)
 
-            ### Creating the node
+            # Creating the node
             url = "http://%s:%s/v2/projects/%s/appliances/%s" % \
                    (CONFIG["gns3_server"], CONFIG["gns3_port"], \
                    CONFIG["project_id"], appliance["appliance_id"])
@@ -105,8 +104,9 @@ def add_nodes():
                      (response.status_code, instance["name"]))
                 exit(1)
 
-    ### Retrieving all nodes in the project, the assigning node IDs and console port numbers
-    ### by searching the node's name, then appending the config with them.
+    # Retrieving all nodes in the project, the assigning node IDs and console
+    # port numbers by searching the node's name, then appending the config with
+    # them.
     url = "http://%s:%s/v2/projects/%s/nodes" % \
            (CONFIG["gns3_server"], CONFIG["gns3_port"], CONFIG["project_id"])
     response = get(url)
@@ -122,7 +122,6 @@ def add_nodes():
     else:
         print("Received HTTP error %d when retrieving nodes! Exiting." % response.status_code)
         exit(1)
-
 
 
 def add_links():
@@ -148,13 +147,20 @@ def add_links():
 
         url = "http://%s:%s/v2/projects/%s/links" % \
                (CONFIG["gns3_server"], CONFIG["gns3_port"], CONFIG["project_id"])
-        data = {"nodes": [ \
-               {"node_id": link[0]["node_id"], \
-               "adapter_number": link[0]["adapter_number"], \
-               "port_number": link[0]["port_number"]}, \
-               {"node_id": link[1]["node_id"], \
-               "adapter_number": link[1]["adapter_number"], \
-               "port_number": link[1]["port_number"]}]}
+        data = {
+            "nodes": [
+               {
+                   "node_id": link[0]["node_id"],
+                   "adapter_number": link[0]["adapter_number"],
+                   "port_number": link[0]["port_number"]
+               },
+               {
+                   "node_id": link[1]["node_id"],
+                   "adapter_number": link[1]["adapter_number"],
+                   "port_number": link[1]["port_number"]
+               }
+            ]
+        }
 
         data_json = dumps(data)
         response = post(url, data=data_json)
@@ -164,7 +170,6 @@ def add_links():
                   link[0]["node_id"], link[0]["adapter_number"], link[0]["port_number"], \
                   link[1]["node_id"], link[1]["adapter_number"], link[1]["port_number"]))
             exit(1)
-
 
 
 def start_nodes():
@@ -182,7 +187,6 @@ def start_nodes():
         exit(1)
 
 
-
 def day0_config():
     """
     Deploying Day-0 configuration
@@ -197,7 +201,6 @@ def day0_config():
                 call(expect_cmd)
 
 
-
 def build_ansible_hosts():
     """
     Creating an Ansible hosts file from the nodes
@@ -209,51 +212,50 @@ def build_ansible_hosts():
                 # Creating inventory groups based on OS
                 hosts_file.write("[%s]\n" % appliance["os"])
                 for instance in appliance["instances"]:
-                    # Writing the hostname and its IP address to the inventory file. The sub
-                    # function reremoves the /xx or " xxx.xxx.xxx.xxx" portion of the address.
-                    hosts_file.write("%s ansible_host=%s\n" % \
-                                    (instance["name"], sub("/.*$| .*$", "", instance["ip"])))
+                    # Writing the hostname and its IP address to the inventory
+                    # file. The sub function reremoves the /xx or
+                    # "xxx.xxx.xxx.xxx" portion of the address.
+                    hosts_file.write(
+                        "%s ansible_host=%s\n" % \
+                        (instance["name"], sub("/.*$| .*$", "", instance["ip"]))
+                    )
                 hosts_file.write("\n")
 
 
-
-
-
 if __name__ == "__main__":
-
-    ### Loading config file
+    # Loading config file
     with open("topology_config.yml") as config_file:
         CONFIG = load(config_file)
 
-    ### Create project and add its ID to the config
+    # Create project and add its ID to the config
     print("Creating GNS3 project")
     create_project(CONFIG["project_name"])
 
-    ### Add appliance IDs to the config
+    # Add appliance IDs to the config
     print("Retrieving appliance IDs")
     assign_appliance_id()
 
-    ### Add nodes to the topology
+    # Add nodes to the topology
     print("Adding nodes")
     add_nodes()
 
-    ### Create links between the nodes
+    # Create links between the nodes
     print("Adding links")
     add_links()
 
-    ### Creating inventory file for Ansible
+    # Creating inventory file for Ansible
     print("Generating Ansible inventory file")
     build_ansible_hosts()
 
-    ### Dump final config into "topology_full.yml"
+    # Dump final config into "topology_full.yml"
     print("Saving final topology config.")
     with open("topology_full.yml", 'w+') as topology_file:
         safe_dump(CONFIG, topology_file, default_flow_style=False)
 
-    ### Start nodes
+    # Start nodes
     print("Starting nodes")
     start_nodes()
 
-    ### Day-0 configuration
+    # Day-0 configuration
     print("Applying Day-0 configuration")
     day0_config()
